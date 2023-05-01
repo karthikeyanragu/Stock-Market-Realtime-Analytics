c8a3909b698a7c64ba30daf629f52f5f88892d60
Karthikeyan R <104515955+karthikeyanragu@users.noreply.github.com>
2023-05-01 18:22:43 +0530

    Add files via upload

---
+++ Bigquery_table_ddl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `persuasive-pipe-384604.bigdata.streaming_stock`
+(
+  symbol STRING,
+  timestamp TIMESTAMP,
+  open FLOAT64,
+  high FLOAT64,
+  low FLOAT64,
+  close FLOAT64,
+  volume INT64
+);
